94a261a17143d2a48929f2f270c6314d505fd1d1
Evan0769 <evan0769@gmail.com>
2021-02-08 13:05:51 +0000

    侧边栏链接

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -42,13 +42,14 @@ class Topic extends Model
     {
         // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
         // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
-        return $query->orderBy('updated_at', 'desc');
+       // return $query->orderBy('updated_at', 'desc');
+       return $query->orderBy('updated_at', 'desc')->with('user', 'category');
     }
 
     public function scopeRecent($query)
     {
         // 按照创建时间排序
-        return $query->orderBy('created_at', 'desc');
+        return $query->orderBy('created_at', 'desc')->with('user', 'category');
     }
     public function link($params = [])
     {
